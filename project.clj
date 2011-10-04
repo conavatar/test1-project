83e7dc0c0c3869615ff4d0da4877e8868a9fc781
conavatar <conavatgar@gmail.com>
2011-10-04 07:52:09 +0530

    First commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject test1-project "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [incanter "1.2.3-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :main test1-project.core)
---
+++ test/test1_project/test/core.clj
@@ -0,0 +1,6 @@
+(ns test1-project.test.core
+  (:use [test1-project.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
